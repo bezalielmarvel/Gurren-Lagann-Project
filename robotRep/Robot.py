18c0e2379fdf2e080eccc9168b5d654ec005ca35
ilyesB <ilyes.boussama@yahoo.fr>
2018-02-10 15:13:25 +0100

    fichier oublie + petits changements

--- robotRep/Robot.py
+++ robotRep/Robot.py
@@ -32,16 +32,20 @@ class Robot(Objet3D):
         """
         deplace le robot dans le sens voulu (1 pour l'avant, -1 pour l'arriere), sur sa direction
         """
-        self.deplacer(self.direction*self.vitesse*sens)
+        if sens<0:
+            self.deplacer(self.direction*-self.vitesse)
+        elif sens>0:
+            self.deplacer(self.direction*self.vitesse)
 
     def tourner(self, sens):
         """
         tourne le robot par rapport a une des roues selon le sens 
         """
-        self.direction.rotation2D(-sens*self.vitesseRot)
-        if sens==-1:
+        if sens<0:
+            self.direction.rotation2D(self.vitesseRot)
             self.tournerAutour(self.rd.centre, self.vitesseRot)
-        elif sens==1:
+        elif sens>0:
+            self.direction.rotation2D(-self.vitesseRot)
             self.tournerAutour(self.rg.centre, -self.vitesseRot)
     
     def tournerAutour(self, point, angle):
