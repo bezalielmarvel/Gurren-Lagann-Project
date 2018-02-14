13f4462cefbd5c3b805a1ccfadf3fe7be9fe45d1
Unknown <alexandre95870@yahoo.fr>
2018-02-14 16:09:11 +0100

    Ajout du debut de la classe test robot
    correction de la facon d'ecrire le code pour respecter les conventions d'écriture

--- robotRep/Robot.py
+++ robotRep/Robot.py
@@ -16,35 +16,36 @@ class Robot(Objet3D):
         rg: Objet3D, roue gauche
         """
         Objet3D.__init__(self)
-        self.direction=direction
-        self.vitesse=0.0
-        self.vitesseRot=0.0
-        self.forme=pave
-        self.centre=pave.centre #initalise le centre au centre du pave
-        self.rd=rd
-        self.rg=rg
+        self.direction = direction
+        self.vitesse = 0.0
+        self.vitesseRot = 0.0
+        self.forme = pave
+        self.centre = pave.centre #initalise le centre au centre du pave
+        self.rd = rd
+        self.rg = rg
         
         #initialisation des centres des roues
-        self.rd.centre=pave.sommets[1]/2
-        self.rg.centre=pave.sommets[0]/2        
+        self.rd.centre = pave.sommets[1]/2
+        self.rg.centre = pave.sommets[0]/2
 
     def avancer(self, sens):
         """
         deplace le robot dans le sens voulu (1 pour l'avant, -1 pour l'arriere par ex), sur sa direction
+        La fonction deplacer vien du module Vecteur eet se trouve dans la class point
         """
-        if sens<0:
+        if sens < 0:
             self.deplacer(self.direction*-self.vitesse)
-        elif sens>0:
+        elif sens > 0:
             self.deplacer(self.direction*self.vitesse)
 
     def tourner(self, sens):
         """
         tourne le robot par rapport a une des roues selon le sens 
         """
-        if sens<0:
+        if sens < 0:
             self.direction.rotation2D(self.vitesseRot)
             self.tournerAutour(self.rd.centre, self.vitesseRot)
-        elif sens>0:
+        elif sens > 0:
             self.direction.rotation2D(-self.vitesseRot)
             self.tournerAutour(self.rg.centre, -self.vitesseRot)
     
