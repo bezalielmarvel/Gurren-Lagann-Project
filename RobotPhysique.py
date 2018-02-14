0230f7bcf3122e7eed1116675b2921c39ad4216e
aitalibraham <35796336+aitalibraham@users.noreply.github.com>
2018-02-14 22:02:46 +0000

    Delete RobotPhysique.py

--- RobotPhysique.py
+++
@@ -1,9 +0,0 @@
-from robotRep.Robot import *
-from robotRep.Tete import *
-
-class RobotPhysique (Robot):
-    def __init__(self , pave , rg ,rd , direction ):
-        Robot.__init__(pave , rg , rd , direction)
-        self.tete = Tete(pave.centre , direction) # la tete est exactement au centre du robot, elle est orienté comme le vecteur direction 
-
-    
