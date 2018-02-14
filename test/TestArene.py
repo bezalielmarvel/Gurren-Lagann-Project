fca3d6d1b9e63bce3cf68ab612705ec7ac244450
Unknown <alexandre95870@yahoo.fr>
2018-02-14 10:50:30 +0100

    Ajout de la class testArene avec unittest
    Implementation des tests avec unittest sur la class arene

--- test/TestArene.py
+++ test/TestArene.py
@@ -1,18 +1,37 @@
-from geometrie3D import Arene, Objet3D
+import unittest
+
+from geometrie3D import Arene
+from geometrie3D import Polygone3D
+from geometrie3D import Objet3D
+from geometrie3D import Point
 
 
 
-a=Arene()
-n=3
-b=Objet3D()
+class testArene(unittest.TestCase):
 
-for i in range(0,n):
-    a.add(Objet3D())
-print(a)
+    def setUp(self):
+        self.a = Arene()
+        self.n = 3
+        self.b = Objet3D()
 
-#on vide l'arene 
-a.vider()
-print(a)
 
-a.add(b)
-print(a)
+    def test_creation_arene(self):
+        self.assertIsInstance(self.a, Arene, msg=None)
+        self.assertIsInstance(self.b, Objet3D, msg=None)
+        self.assertEqual(len(self.a.objets3D), 0)
+
+
+    def test_arene_remplie(self):
+        self.assertEqual(len(self.a.objets3D), 0)
+        for i in range(0, self.n):
+            self.a.add(Objet3D())
+        self.assertEqual(len(self.a.objets3D), self.n)
+
+    def test_vide_arene(self):
+        for i in range(0, self.n):
+            self.a.add(Objet3D())
+        print(len(self.a.objets3D))
+        self.a.vider()
+        self.assertEqual(len(self.a.objets3D), 0)
+
+
