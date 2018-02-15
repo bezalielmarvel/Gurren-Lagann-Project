9e7034ac4de47566a887ea084535fd91f33abe82
Unknown <alexandre95870@yahoo.fr>
2018-02-15 10:53:04 +0100

    Ajout de l'unnitest pour trie objet
    unnitest pout triObjet

--- test/testTriObjets.py
+++ test/testTriObjets.py
@@ -2,56 +2,64 @@ from geometrie3D import Objet3D
 from geometrie3D import Polygone3D
 from geometrie3D.Arene import *
 from geometrie3D.pointRep import *
+import unittest
 
-o1=Polygone3D()
-o2=Polygone3D()
-o3=Polygone3D()
+class TestTrieObjet(unittest.TestCase):
 
-a=Point(0,0,0)
-b=Point(25,25,25)
-c=Point(50,50,50)
-d=Point(75,75,75)
+    def setUp(self):
+        self.a1 = Arene()
+        self.a2 = Arene()
+        self.a3 = Arene()
+        self.o1 = Polygone3D()
+        self.o2 = Polygone3D()
+        self.o3 = Polygone3D()
+        self.a = Point(100, 100, 100)
+        self.b = Point(125, 125, 125)
+        self.c = Point(150, 150, 150)
+        self.d = Point(175, 175, 175)
+        self.o1.addSommet(self.a)
+        self.o1.addSommet(self.b)
+        self.o1.addSommet(self.c)
+        self.o1.addSommet(self.d)
+        self.e = Point(200, 200, 200)
+        self.f = Point(225, 225, 225)
+        self.g = Point(250, 250, 250)
+        self.h = Point(275, 275, 275)
+        self.o2.addSommet(self.e)
+        self.o2.addSommet(self.f)
+        self.o2.addSommet(self.g)
+        self.o2.addSommet(self.h)
+        self.i = Point(300, 300, 300)
+        self.j = Point(325, 325, 325)
+        self.k = Point(350, 350, 350)
+        self.l = Point(375, 375, 375)
+        self.o3.addSommet(self.i)
+        self.o3.addSommet(self.j)
+        self.o3.addSommet(self.k)
+        self.o3.addSommet(self.l)
+        self.a1.add(self.o1)
+        self.a1.add(self.o2)
+        self.a1.add(self.o3)
+        self.a2.add(self.o3)
+        self.a2.add(self.o2)
+        self.a2.add(self.o1)
+        self.a3.add(self.o3)
+        self.a3.add(self.o1)
+        self.a3.add(self.o2)
 
-o1.addSommet(a)
-o1.addSommet(b)
-o1.addSommet(c)
-o1.addSommet(d)
 
-a=Point(100,100,100)
-b=Point(125,125,125)
-c=Point(150,150,150)
-d=Point(175,175,175)
+    def test_trie(self):
+        self.assert
 
-o2.addSommet(a)
-o2.addSommet(b)
-o2.addSommet(c)
-o2.addSommet(d)
 
-a=Point(200,200,200)
-b=Point(225,225,225)
-c=Point(250,250,250)
-d=Point(275,275,275)
 
-o3.addSommet(a)
-o3.addSommet(b)
-o3.addSommet(c)
-o3.addSommet(d)
 
-a1=Arene()
-a2=Arene()
-a3=Arene()
 
-a1.add(o1)
-a1.add(o2)
-a1.add(o3)
 
-a2.add(o3)
-a2.add(o2)
-a2.add(o1)
 
-a3.add(o3)
-a3.add(o1)
-a3.add(o2)
+
+
+
 
 print("\nAvant le tri:\n")
 print(a1,"\n")
