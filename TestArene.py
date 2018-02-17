d53b8d32fc0f3e1c17ae9f131f88a45c9da2d034
aitalibraham <35796336+aitalibraham@users.noreply.github.com>
2018-02-17 19:23:09 +0100

    Add files via upload
    permet de tester les fonctions de lecture et d'ecriture de l'arene dans un fichier.

---
+++ TestArene.py
@@ -0,0 +1,15 @@
+from geometrie3D.Arene import *
+from geometrie3D.Polygone3D import *
+from geometrie3D.Pave import *
+
+
+#Creation d'une arene et ajout de n objets
+a = Arene()
+p = Pave(45,3,12)
+poly = Polygone3D()
+poly.sommets = [Point(14,15,14) , Point(5,99,120)]
+a.add(poly)
+a.add(p)
+a.sauvegarder("./geometrie3D/BanqueArenes/arene1.txt")
+b = Arene()
+a.lecture_fichier("./geometrie3D/BanqueArenes/arene1.txt")
