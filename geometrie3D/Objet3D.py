d68d1a208158b3141c48447174ab8777bc90b546
aitalibraham <35796336+aitalibraham@users.noreply.github.com>
2018-02-17 19:21:46 +0100

    Add files via upload
    j'ai du modifier les import, ca a posait plusieurs problèmes avant.

--- geometrie3D/Objet3D.py
+++ geometrie3D/Objet3D.py
@@ -1,4 +1,5 @@
-from geometrie3D.pointRep import Point
+from .pointRep import Point
+
 
 class Objet3D(object):
     """
@@ -10,13 +11,13 @@ class Objet3D(object):
         centre : Point definissant le centre de l'objet. initialise a (0, 0, 0)
         """
         self.centre = Point(0, 0, 0)
-        
+
     def deplacer(self, vecteur):
         """
         deplace les Point dans sommets et centre de l'objet
         """
         self.centre.deplacer(vecteur)
-        
+
     def __repr__(self):
         """
         Quand on entre un objet3D dans l'interpreteur
@@ -26,7 +27,7 @@ class Objet3D(object):
     def __getattr__(self, nom):
         """
         Permet d'acceder a un attribut
-        
+
         si ce n'est pas possible:
         """
         print("L'attribut {} n'est pas accessible dans {} !".format(nom, type(self)))
