455fd3a86acbee2640ef81e4badb5133d39b9688
aitalibraham <35796336+aitalibraham@users.noreply.github.com>
2018-02-19 00:50:10 +0100

    Add files via upload
    j'ai effectué quelques changement sur la fonction vueDessus, le temps de calcule est énorme, faudrait trouver une autre solution

--- geometrie3D/Arene.py
+++ geometrie3D/Arene.py
@@ -1,139 +1,116 @@
-from geometrie3D.Objet3D import *
-from geometrie3D import Polygone3D
-from geometrie3D import pointDansPolygone
+from .Objet3D import *
+from .Pave import *
+from .Polygone3D import *
+from .trouverObjet3D import *
+from .pointDansPolygone import *
 
 class Arene(object):
-    """
-    Definit une structure de base pour une arene contenant des Objet3D
-    """
+	"""
+	Definit une structure de base pour une arene contenant des Objet3D
+	"""
 
-    def __init__(self):
-        """
-        objets3D: [Objet3D]
-        """
-        self.objets3D = list()
+	def __init__(self):
+		"""
+		objets3D: [Objet3D]
+		"""
+		self.objets3D = list()
 
-    def add(self, objet3D):
-        """
-        Ajoute un objet3D a la liste si c'est une sous classe de Objet3D
-        """
-        if (issubclass(type(objet3D), Objet3D)):
-            self.objets3D.append(objet3D)
+	def add(self, objet3D):
+		"""
+		Ajoute un objet3D a la liste si c'est une sous classe de Objet3D
+		"""
+		if (issubclass(type(objet3D), Objet3D)):
+			self.objets3D.append(objet3D)
 
-    def vider(self):
-        """
-        Reinitialise la liste d'objets 3D
-        """
-        self.objets3D = list()
-
-
-    def vueDessus(self, xmax, ymax):
-        matrice2D = [[-1] * ymax for _ in range(xmax)]
-        resolutionx = 0
-        resolutiony = 0
-
-        while(xmax/10):
-            resolutionx += 1
-        while(ymax/10):
-            resolutiony += 1
-
-        for a in self.objets3D:
-            if isinstance(a, Polygone3D):
-                listeSommets = a.sommets
-                listeCoord = listeSommets[0]
-                max_x = listeCoord.x
-                max_y = listeCoord.y
-                for elt in listeSommets:
-                    """on recupere chaques coordonnées x, y, z de elt qui parcourt la liste de sommet"""
-                    listeCoord = elt
-                    if (listeCoord.x > max_x):
-                        max_x = listeCoord.x
-                    if (listeCoord.y > max_y):
-                        max_y = listeCoord.y
-            for i in range (listeCoord.x*(resolutionx*10), max_x * (resolutionx*10)):
-                for j in range(listeCoord.y * (resolutiony*10), max_y * (resolutionx*10)):
-                    matrice2D[i][j] = 1
+	def vider(self):
+		"""
+		Reinitialise la liste d'objets 3D
+		"""
+		self.objets3D = list()
 
 
-    def sauvegarder(self , nomfichier):
-        f = open(nomfichier , "w")
-        f.write("1024 1024\n")
-        for objet in self.objets3D :
-            if issubclass(type(objet) , Pave) :
-                f.write("PAVE {} {} {}".format(objet.longueur , objet.largeur,objet.hauteur))
-            else :
-                f.write("POLYGONE3D")
-                for som in objet.sommets :
-                    f.write(" ({},{},{})".format(som.x , som.y , som.z))
-            f.write("\n")
-        f.close()
+	def vueDessus(self, xmax, ymax):
+		matrice2D = [[-1] * ymax for _ in range(xmax)]
+		resolutionx = 0
+		resolutiony = 0
+		maximumx = xmax
+		maximumy = ymax
+		boolean = False
+		listeSommets = []
 
-    def lecture_fichier(self, fichier):
-        """
-        Cette cree une arene avec
-        obstacles declaré dans un fichier txt.
-        """
+		"""On regarde combien de chiffres possede xmax et ymax pour determiner la resolution de la matrice"""
+		while(maximumx):
+			resolutionx += 1
+			maximumx = maximumx/10
+		while(maximumx):
+			resolutiony += 1
+			maximumy = maximumy/10
 
-        mon_fichier = open(fichier, "r")
-        for line in mon_fichier.read.splitlines()[1:]:
-            words = line.split();
-            if (words[0] == "POLYGONE3D"):
-                polygone = Polygone3D()
-                for sommet_str in words[2:]:
-                    sommet_str = sommet.replace("(", "")
-                    sommet_str = sommet.replace(")", "")
-                    sommet_tab = sommet_str.split(",")
-                    polygone.add(Point(sommet_tab[0], sommet_tab[1], sommet_tab[2]))
-                self.add(polygone)
-            elif (word[0] == "PAVE"):
-                pave = Pave()
-                for sommet in words[2:]:
-                    sommet_str = sommet.replace("(", "")
-                    sommet_str = sommet.replace(")", "")
-                    sommet_tab = sommet_str.split(",")
-                    pave.add(Point(sommet_tab[0], sommet_tab[1], sommet_tab[2]))
-                self.add(pave)
-        mon_fichier.close()
+		for a in self.objets3D:
+			if isinstance(a, Polygone3D):
+				listeSommets = a.sommets
+				if len(listeSommets) > 0 :
+					for i in range(0, xmax * (resolutionx*10)):
+						for j in range(0, ymax * (resolutionx*10)):
+							boolean = point_inside_polygon(i, j, listeSommets)
+							if(boolean):
+								matrice2D[i][j] = 1
+								print(boolean)
+							print(i,j)
 
-    def __repr__(self):
-        """
-        Quand on entre une arene dans l'interpreteur
-        """
-        return "Arene: objets3D({})".format(self.objets3D)
+	def sauvegarder(self , nomfichier):
+		f = open(nomfichier , "w")
+		f.write("1024 1024\n")
+		for objet in self.objets3D :
+			if issubclass(type(objet) , Pave) :
+				f.write("PAVE {} {} {}".format(objet.longueur , objet.largeur,objet.hauteur))
+			else :
+				f.write("POLYGONE3D")
+				for som in objet.sommets :
+					f.write(" ({},{},{})".format(som.x , som.y , som.z))
+			f.write("\n")
+		f.close()
+		
 
-    def __getattr__(self, nom):
-        """
-        Permet d'acceder a un attribut
+	def lecture_fichier(self, fichier):
+		"""
+		Cette cree une arene avec
+		obstacles declaré dans un fichier txt.
+		"""
 
-        si ce n'est pas possible:
-        """
-        print("L'attribut {} n'est pas accessible dans Arene !".format(nom))
+		mon_fichier = open(fichier, "r")
+		for line in mon_fichier.read().splitlines()[1:]:
+			words = line.split();
+			if (words[0] == "POLYGONE3D"):
+				polygone = Polygone3D()
+				for sommet_str in words[1:]:
+					sommet_str = sommet.replace("(", "")
+					sommet_str = sommet_str.replace(")", "")
+					sommet_tab = sommet_str.split(",")
+					polygone.add(Point(int(sommet_tab[0]), int(sommet_tab[1]), int(sommet_tab[2])))
+				self.add(polygone)
+			elif (word[0] == "PAVE"):
+				pave = Pave()
+				for sommet in words[1:]:
+					sommet_str = sommet.replace("(", "")
+					sommet_str = sommet_str.replace(")", "")
+					sommet_tab = sommet_str.split(",")
+					pave.add(Point(int(sommet_tab[0]), int(sommet_tab[1]), int(sommet_tab[2])))
+				self.add(pave)
+		mon_fichier.close()
+	def __repr__(self):
+		"""
+		Quand on entre une arene dans l'interpreteur
+		"""
+		return "Arene: objets3D({})".format(self.objets3D)
+
+	def __getattr__(self, nom):
+		"""
+		Permet d'acceder a un attribut
+
+		si ce n'est pas possible:
+		"""
+		print("L'attribut {} n'est pas accessible dans Arene !".format(nom))
 
 
 
-
-
-    def vueDessus(self, xmax, ymax):
-        matrice2D = [[-1] * ymax for _ in range(xmax)]
-        resolutionx = 0
-        resolutiony = 0
-        maximumx = xmax
-        maximumy = ymax
-        boolean = False
-
-        """On regarde combien de chiffres possede xmax et ymax pour determiner la resolution de la matrice"""
-        while(maximumx):
-            resolutionx += 1
-            maximumx = maximumx/10
-        while(maximumx):
-            resolutiony += 1
-            maximumy = maximumy/10
-
-        for a in self.objets3D:
-            if isinstance(a, Polygone3D):
-                listeSommets = a.sommets
-            for i in range(0, xmax * (resolutionx*10)):
-                for j in range(0, ymax * (resolutionx*10)):
-                    boolean = pointDansPolygone(i, j, listeSommets)
-                    if(boolean):
-                        matrice2D[i][j] = 1
