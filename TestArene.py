77420b75a88f6edf5d41e983fa69a3653f819223
aitalibraham <35796336+aitalibraham@users.noreply.github.com>
2018-02-19 00:51:02 +0100

    Add files via upload

--- TestArene.py
+++ TestArene.py
@@ -1,7 +1,7 @@
 from geometrie3D.Arene import *
 from geometrie3D.Polygone3D import *
 from geometrie3D.Pave import *
-
+from geometrie3D.trouverObjet3D import *
 
 #Creation d'une arene et ajout de n objets
 a = Arene()
@@ -10,7 +10,7 @@ poly = Polygone3D()
 poly.sommets = [Point(14,15,14) , Point(5,99,120)]
 a.add(poly)
 a.add(p)
-#a.sauvegarder("./geometrie3D/BanqueArenes/arene1.txt")
+a.sauvegarder("./geometrie3D/BanqueArenes/arene1.txt")
 b = Arene()
 #a.lecture_fichier("./geometrie3D/BanqueArenes/arene1.txt")
-a.vueDessus(200,200)
+a.vueDessus(5,5)
