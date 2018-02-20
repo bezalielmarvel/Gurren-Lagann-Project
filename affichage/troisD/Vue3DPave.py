0cd85cb0c3f3574e8379c904cbb04a7b73e172c0
ilyesB <ilyes.boussama@yahoo.fr>
2018-02-20 14:57:18 +0100

    ajout commentaires

--- affichage/troisD/Vue3DPave.py
+++ affichage/troisD/Vue3DPave.py
@@ -9,17 +9,25 @@ class Vue3DPave(object):
     def __init__(self, pave, batch=None):
         """
         pave : Pave        
-        vertex_list: VertexList (pyglet) 
+        sommets_data: Tuple
+        self.index: [ int ]
         """
         self.pave=pave
-        self.sommets_data=()
+        
+        #definit les cotes avec kles indices des sommets: (0,1), (0,3) ...
         self.index=[0, 1, 0, 3, 0, 4, 1, 2, 1, 5, 2, 3, 2, 6, 3, 7, 4, 5, 4, 7, 5, 6, 6, 7]
+        
+        #concatenation des coordonnees des sommets
+        self.sommets_data=()        
         for i in range(0, len(self.pave.sommets)):
             self.sommets_data+=self.pave.sommets[i].toTuple()
                        
     def draw(self):
+        """
+        dessine le pave dans la fenetre courante
+        """
         pyglet.graphics.draw_indexed(8, pyglet.gl.GL_LINES, 
-                                     self.index, ('v3f', self.sommets_data), ('c3B', (255,255,255)*8) )
+                                     self.index, ('v3f', self.sommets_data), ('c3B', (255,255,255)*8) ) # les lignes sont blanches
     def update(self):
         """
         met a jour les coordonnees du pave
